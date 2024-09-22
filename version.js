584000c7b1b898721132a9d51d949662a1aced77
Rickard Armiento <gitcommits@armiento.net>
2024-09-22 20:02:31 +0200

    Update version.js

--- version.js
+++ version.js
@@ -1,2 +1,2 @@
-const VERSION = '1.0.0';  // Change this version number when you update your app
+const VERSION = '1.0.1';  // Change this version number when you update your app
 export default VERSION;
